470fb89c706d605b433eb4cd2737f4324a1b5faf
Kovalyuk <beback95@gmail.com>
2019-02-28 11:12:29 +0300

    change port on server

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const config = require('../../config/webpack/development');
 const routes = require('./routes');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const compiler = webpack(config);
 
 if (process.env.NODE_ENV === 'development') {
